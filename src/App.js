f7ca564ed09feef2613403883b39f4bf1a39f8d3
AMINUL ISLAM <lostvayne684@gmail.com>
2022-12-29 13:39:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import React from 'react';
+import React from "react";
 
 import {
   BrowserRouter,
   Routes, // instead of "Switch"
   Route,
 } from "react-router-dom";
-import Register from './components/Register';
+import Documents from "./components/Documents";
+import Pages from "./components/Pages";
+
+
 
 function App() {
   return (
     <>
-
       <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={<Register />} />
+          <Route exact path="/" element={<Documents />} />
+        </Routes>
+        <Routes>
+          <Route exact path="/Pages/:id" element={<Pages />} />
         </Routes>
       </BrowserRouter>
-
     </>
   );
 }
